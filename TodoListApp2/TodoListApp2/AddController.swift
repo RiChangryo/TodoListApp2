abc10d3d41ef26b4cdc964331218c0fbbad82add
RiChangryo <changryo0120@gmail.com>
2018-12-13 09:45:44 +0900

    Step２途中　追加画面紐付け前まで完成

---
+++ TodoListApp2/TodoListApp2/AddController.swift
@@ -0,0 +1,36 @@
+//
+//  AddController.swift
+//  TodoListApp2
+//
+//  Created by 短期学部 on 2018/12/13.
+//  Copyright © 2018年 RiChangryo. All rights reserved.
+//
+
+import UIKit
+var TodoKobetsunonakami = [String]()
+
+class AddController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
